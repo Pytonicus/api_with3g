a23d125d130a410a11cbaec07ddacef7c0a436a2
Guillermo <guillermogranadosgomez@gmail.com>
2022-01-21 16:27:10 +0100

    personalizar titulo api

--- api_with3g/urls.py
+++ api_with3g/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "API With3G"
+admin.site.index_title = "Configuración API With3G"
+admin.site.site_title = "Configuración API With3G"
